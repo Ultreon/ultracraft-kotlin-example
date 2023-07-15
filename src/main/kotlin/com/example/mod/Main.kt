705eb2706083385bd6883e799fcb2649c33e9f1b
Qboi123 <qboiwastaken@gmail.com>
2023-07-15 16:48:45 +0200

    E

--- src/main/kotlin/com/example/mod/Main.kt
+++ src/main/kotlin/com/example/mod/Main.kt
@@ -3,6 +3,9 @@ package com.example.mod
 import com.example.mod.init.ModBlocks
 import com.ultreon.craft.BlockModelRegistry
 import com.ultreon.craft.GamePlatform
+import com.ultreon.craft.events.BlockEvents
+import com.ultreon.craft.events.WorldEvents
+import com.ultreon.craft.registry.Registries
 import net.fabricmc.api.ModInitializer
 import org.slf4j.Logger
 
@@ -15,6 +18,12 @@ object Main : ModInitializer {
 
         ModBlocks.register()
         BlockModelRegistry.registerDefault(ModBlocks.example)
+
+        BlockEvents.SET_BLOCK.listen { world, pos, block ->
+            if (block.isAir) return@listen
+
+            world[pos] = Registries.BLOCK.values().random()
+        }
     }
 
 }
