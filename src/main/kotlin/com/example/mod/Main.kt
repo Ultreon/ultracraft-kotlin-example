2305e07385577cbf1736cbe0f3a9827d9f02bf65
Qboi123 <qboiwastaken@gmail.com>
2023-07-15 19:35:28 +0200

    Update UltraCraft version fix metadata and improve main class

--- src/main/kotlin/com/example/mod/Main.kt
+++ src/main/kotlin/com/example/mod/Main.kt
@@ -3,6 +3,9 @@ package com.example.mod
 import com.example.mod.init.ModBlocks
 import com.ultreon.craft.BlockModelRegistry
 import com.ultreon.craft.GamePlatform
+import com.ultreon.craft.entity.Player
+import com.ultreon.craft.events.LifecycleEvents
+import com.ultreon.libs.collections.v0.util.ArrayUtils
 import net.fabricmc.api.ModInitializer
 import org.slf4j.Logger
 
@@ -15,5 +18,9 @@ object Main : ModInitializer {
 
         ModBlocks.register()
         BlockModelRegistry.registerDefault(ModBlocks.example)
+
+        LifecycleEvents.GAME_LOADED.listen {
+            Player.allowed = ArrayUtils.add(Player.allowed, ModBlocks.example.get())
+        }
     }
 }
